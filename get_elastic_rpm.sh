8cdd764acec27fbd3b37131b36a248b9ed13cace
Aleksandr Eroshkin <aleksandr_eroshkin@epam.com>
2019-11-19 21:16:34 +0400

    Move all variables into group_vars

---
+++ get_elastic_rpm.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+export ELASTIC_VER="7.3.2"
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_VER}-x86_64.rpm -O ./roles/elastic_install/files/elasticsearch.rpm
