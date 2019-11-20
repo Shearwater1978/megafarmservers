fbe21ff269772e6ce4f532d1f84eba376c14f9ef
Aleksandr Eroshkin <aleksandr_eroshkin@epam.com>
2019-11-20 13:42:45 +0000

    Rebuild process for getting consul token

--- get_elastic_rpm.sh
+++ get_elastic_rpm.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
 export ELASTIC_VER="7.3.2"
+mkdir -p ./roles/elastic_install/files/
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_VER}-x86_64.rpm -O ./roles/elastic_install/files/elasticsearch.rpm
